168536d4b7d789e003b9e9516e8715b25f6f539f
Jherson Dignadice <j.dignadice.27@gmail.com>
2022-05-19 18:12:18 +0800

    add activity code s46

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -4,17 +4,20 @@ import React, {useState} from 'react'
 // Bootstrap Components
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row' 
-import Col from 'react-bootstrap/Col' 
 import Container from 'react-bootstrap/esm/Container'
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button' 
 
 
-export default function CourseCard (props) { // props function 
+export default function CourseCard (props) {
+
+    // props function 
     // Props - short term for properties, similar to the arguments/parameters found inside the funmcctions, a way for the parent component to receive information
         // through the use of props, decvs can sue the same compoonent and feed different info/ data for rendering
+    
     let course = props.course
 
     /* 
+
        useState() used in react ot allow components to create manage its own data  and meant to be managed internally.
         - accepts an argument that is meant to be the value of the firs element in array
 
@@ -25,8 +28,8 @@ export default function CourseCard (props) { // props function
 
        setCount function is used to update the value of th count variable, depending on the times that the enroll function is triggered by the onClick command (button event)
 
-       
     */
+
     const [count, setCount] = useState(0)
     const maxCount = 30
 
@@ -42,30 +45,35 @@ export default function CourseCard (props) { // props function
     return (
         <Row>
            
-        <Container fluid>
-                <Card className = "course-Card">
-                    <Card.Body>
+            <Container fluid>
 
-                        <Card.Title>
-                            <h2>{course.name}</h2>
-                        </Card.Title>
+                    <Card className = "course-Card">
 
-                        <Card.Text>
-                            <h6>Description</h6>
-                            <p>{course.description}</p>
-                        </Card.Text>
+                        <Card.Body>
 
-                        <Card.Text>
-                          <h6>Price :</h6>
-                          <p>{course.price}</p>
-                          <h6>Enrollees</h6>
-                          <p>{count} Enrollees</p>
-                        </Card.Text> 
+                            <Card.Title>
+                                <h2>{course.name}</h2>
+                            </Card.Title>
 
-                    </Card.Body>
-                    <Button className='course-Button' variant="primary" onClick={enroll}>Enroll Now</Button>
-                </Card>
-        </Container>
+                            <Card.Text>
+                                <h6>Description</h6>
+                                <p>{course.description}</p>
+                            </Card.Text>
+
+                            <Card.Text>
+                                <h6>Price :</h6>
+                                <p>{course.price}</p>
+                                <h6>Enrollees</h6>
+                                <p>{count} Enrollees</p>
+                            </Card.Text> 
+
+                        </Card.Body>
+                        <Button className='course-Button' variant="primary" onClick={enroll}>Enroll Now</Button>
+
+                    </Card>
+
+            </Container>
+
         </Row>
 
     )
@@ -74,3 +82,4 @@ export default function CourseCard (props) { // props function
 
 // set seat limit to 30 
 // activity fo s46 
+
