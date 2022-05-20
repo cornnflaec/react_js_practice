2d12fda629e47849c23767ace464ecc194017a64
Jherson Dignadice <j.dignadice.27@gmail.com>
2022-05-20 18:37:44 +0800

    add activity s47

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -29,8 +29,9 @@ export default function Register() {
 
 
     function register(e){
+        
         e.preventDefault()
-        Swal.fire('Register successful , you may now login')
+        Swal.fire('Log  in Failed')
  
         setEmail('')
         setPassword('')
@@ -40,26 +41,34 @@ export default function Register() {
 
 
     return(
-        <Container>
-            <h1>Login</h1>
-            <Form onSubmit={register}>
+        <Container className="login__page">
 
-                <Form.Group>
-                    <Form.Label>
-                        <h3>Email Address</h3>
-                    </Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                    
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>
-                        <h3>Password</h3>
-                    </Form.Label>
-                    <Form.Control type="Password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                </Form.Group>
-            
-                <Button variant="primary" type="submit" disabled={isDisabled}>Login</Button>
-            </Form>
+              
+                
+                <Form onSubmit={register}>
+                            <h1>
+                                <div className="align-items-center justify-content-center">
+                                Login
+                                </div>
+                            </h1>
+                      
+                    <Form.Group>
+                        <Form.Label>
+                            <h3>Email Address</h3>
+                        </Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>
+                            <h3>Password</h3>
+                        </Form.Label>
+                        <Form.Control type="Password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                    </Form.Group>
+                
+                    <Button variant="primary" type="submit" disabled={isDisabled}>Login</Button>
+                </Form>
+        
         </Container>
     )
 }
